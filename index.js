fcab2679cad0a934e52f04a94979cb36722a50a8
Behran Kankul <behrank@gmail.com>
2018-07-20 23:07:37 +0300

    npm update

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const express = require('express')
+const express = require("express");
+const db = require("./db");
+const app = express();
+const logger = require("./utilities/logger");
+const consoleLogger = logger.init(logger.types.CONSOLE);
+const config = require("./utilities/config");
+const responseCode=require("./utilities/responseCode");
+const response=require("./utilities/response");
+
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
