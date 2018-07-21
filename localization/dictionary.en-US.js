df9355a86f326dc424335c81e3f21408d00e2636
Behran Kankul <behrank@gmail.com>
2018-07-21 15:24:58 +0300

    api update

--- localization/dictionary.en-US.js
+++ localization/dictionary.en-US.js
@@ -3,13 +3,7 @@ const errorMessages = {
     unableToAuthenticate: "invalid token",
     usedToEmail: "email is already in use",
     invalidUser: "invalid username",
-    nullOrEmptyOriginDate: "origin date is missing",
-    nullOrEmptydestinationDate: "destination date is missing",
     nullOrEmptyDestination: "destination location is missing",
-    nullOrEmptyOrigin: "origin location is missing",
-    invalidLuggageCapacity:"invalid weight info",
-    negativeBaggage:"negative weight info",
-    
     systemError: "unexpected error occured"
 };
 
--- localization/dictionary.tr-TR.js
+++ localization/dictionary.tr-TR.js
@@ -3,12 +3,7 @@ const errorMessages = {
     unableToAuthenticate: "geçersiz token",
     usedToEmail: "bu mail adresi başka bir kullanıcı tarafından kullanılıyor",
     invalidUser: "geçersiz kullanıcı bilgisi",
-    nullOrEmptyOriginDate: "ayrılış tarihini boş geçemezsiniz",
-    nullOrEmptydestinationDate: "varış tarihini boş geçemezsiniz",
     nullOrEmptyDestination: "varış noktası seçmelisiniz",
-    nullOrEmptyOrigin: "kalkış noktası seçmelisiniz",
-    invalidLuggageCapacity:"geçersiz bagaj kapasitesi girdiniz",
-    negativeBaggage:"negatif bir bagaj değeri giremezsiniz.",
     systemError: "beklenmedik bir hata oluştu"
 };
 
